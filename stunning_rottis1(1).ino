70a2b1319a7e80aef184f7bc7a8bf6807e76e24a
josegomez13 <jose.gomez13@udea.edu.co>
2021-04-23 19:48:46 -0500

    Finalizacion del proyecto

--- stunning_rottis1(1).ino
+++ stunning_rottis1(1).ino
@@ -7,7 +7,7 @@
 // PROGRAMA
 
 // Declaracion de funciones y variables.
-	int potiValue;
+  int potiValue;
   const int fila[8] = {2,7,19,5,13,18,12,16}; // Vector para las filas
   const int columna[8] = {6,11,10,3,17,4,8,9}; //Vector para las columnas 
   void imagen(char, int);
@@ -16,12 +16,12 @@
   int opcion = 0;
   int contadorTiempo=0;
   int VelAnimacion=0;
-  char LetrasNumeros[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','*','+','-','¿','?','#','(',')','<','>','¡','!','='}; //arreglo con todos los posibles patrones
-int Numpatron;
+  int Numpatron;
   char patrones []={};
   char patron;
   char times[]={};
   int time;
+
 int A[8][8]=  {        {1,1,0,0,0,0,1,1},
 					   {1,0,0,0,0,0,0,1},
   					   {0,0,0,0,0,0,0,0},
@@ -32,7 +32,7 @@ int A[8][8]=  {        {1,1,0,0,0,0,1,1},
   					   {0,0,0,1,1,0,0,0},
                };
 
-int bitA[] = {0,0,0,0,0,0,0,0};
+int bitA[] = {0,0,0,0,0,0,0,0}; 
 
 int E[8][8]=  {        {0,0,0,0,0,0,0,0},
 					   {0,0,0,0,0,0,0,0},
@@ -74,9 +74,6 @@ int U[8][8]=  {        {1,0,0,1,1,0,0,1},
   					   {1,0,0,0,0,0,0,1},
   					   {1,0,0,0,0,0,0,1},
                };
-//t LetrasMatriz[5][8][8];
-//trasMatriz[0]=A;
-
 
 
 void setup(){
@@ -97,26 +94,58 @@ void setup(){
       digitalWrite(columna[i], HIGH); 	// matriz catodo comun, así que ponemos en high todas las columnas(catodos)
   
 	}
-Serial.println(" MANUAL DE USO.");
-Serial.println(" bla bla bla.");
-Serial.println("Opciones del sistema. ");
-Serial.println("(1). Funcion de verificacion (2). Funcion imagen (3). Funcion publik");
-  
-//cion = Serial.read();
-    
 
+    
+    Serial.println(" MANUAL DE USO.");
+    Serial.println("El presente manual esta organizado de la siguiente  manera:");
+    Serial.println(" ");
+    Serial.println("1. Funcion de verificacion");
+    Serial.println("2. Funcion imagen");
+    Serial.println("3. Funcion publik.");
+    Serial.println("-----------------------------------------");
+    Serial.println("1. Funcion de verificacion");
+    Serial.println("En esta funcion usted podra comprobar que todos los leds de la matriz 8x8 estan funcionando correctamente, encendiendo los 64 leds.");
+    Serial.println("-----------------------------------------");
+    Serial.println("Presiona 1 para continuar ");
+      while(Serial.available()==0){}
+  	Serial.println(" ");
+    Serial.println("2. Funcion imagen.");
+    Serial.println("Esta funcion le permitira mostrar un patron en la matriz de leds. Le pedira ingresar un solo caracter, para esto puede ingresar letras de la A-Z, numeros del 0-9 y caracteres especiales como *, +, -, ¿, ?, #, (, ), <, >, ¡, !, =, luego de ingresarlo podra ver su caracter iluminado en la matriz de leds.");
+    Serial.println("-----------------------------------------");
+    Serial.println("3. Funcion publik");
+    Serial.println("Con esta funcion se le permitira mostrar una secuencia de caracteres. Le pedira ingresar el numero de caracteres que desee iluminar en la matriz led, una vez ingresado le seguira pidiendo caracter por caracter y por cada caracter ingresado le pedira que ingrese un numero de tiempo que sera lo que tardara la visualización entre patrones, hasta completar la cantidad ingresada por usted al inicio de la funcion.");
+    Serial.println(" ");
+
+  
+    //memoria dinamica
  
+  char *pentrada= new char[11];
+  char valor[11]={'B','I','E','N','V','E','N','I','D','O','.'};
+    
+  for(int k=0; k<11; k++){
+    *(pentrada+k)= valor[k];				
+  }
+   for (int k=0; k<11;k++){
+     Serial.print(*(pentrada+k));
+  }
+            
+  delete [] pentrada;
+ 
+Serial.println("Cual de las opciones del sistema desea ejecutar? ");
+Serial.println(" ");
+Serial.println("(1). Funcion de verificacion");
+Serial.println("(2). Funcion imagen");
+Serial.println("(3). Funcion publik");
+  
+
+  
+
   
 
   verificacion();
   
 } // cierre del setup
 
-
- void poti()
-{
-  potiValue = analogRead(0);
-}
 
 
 void loop()
@@ -127,9 +156,7 @@ void loop()
   
   if(Serial.available()>0){
     opcion = Serial.read();
-  	//rificacion();
-    //rial.println(opcion);
-    //rificacion();
+
   
     if(opcion>48 && opcion<=51){
 
@@ -138,23 +165,14 @@ void loop()
          }
 
         else if(opcion == 50){
-			/*for(int i =0; i<8; i++){
-              for(int j =0; j<8; j++){
-                  if(A[i][j]==1){
-                      bitA[i] += pow(2,j);
-                    Serial.println(pow(2,j));
-                    Serial.println(bitA[i]);
-                  }
-              }
-              Serial.println(bitA[i]);
-            }*/
-          for(int k=0; k<8; k++){
-          	bitA[k]=(int)pow(2,k); 
+
+          Serial.print(" Ingrese un solo caracter: ");
+          //Serial.println(patron);
+          while(!Serial.available()){}
+          if(Serial.available()){
+            patron = (char)Serial.read();
+          	imagen(patron);
           }
-          encenderLed(bitA);
-          //Serial.print(" Ingrese un solo caracter: ");
-          //patron = Serial.read();	
-          //imagen(patron);
 
          }
 
@@ -182,13 +200,7 @@ void loop()
   }
 	
   
-  }
-  
-  
-
-
-  
-  
+  }  
   
 } // cierre del Loop
   
@@ -199,60 +211,34 @@ void loop()
 
 
 
-/*void encenderLed(int arreglo[8][8]){ // funcion para encender los led's
-
-  for (int i=0; i<8; i++){
-
-    digitalWrite(fila[i], HIGH); // fila en nivel alto
-
-    for(int j=0; j<8; j++){
-
-      if (arreglo[i][j]== 0){ //condicion para que se encienda el led en esa posicion [i][j]
-
-        digitalWrite(columna[j], LOW); 
-      }  		
-    }
-    delay(2000); //duracion de encendido
-    digitalWrite(fila[i], LOW);
-
-    for(int j=0; j<8; j++){
-      digitalWrite(columna[j], HIGH);
-
-    }  
-  }  
- poti();
-}*/
-
 void encenderLed(int arreglo[8]){
+  
   for (int i=0; i<8; i++){
-    Serial.println((int)pow(2,7+i)+1);
+    
+    //Serial.println((int)pow(2,7+i)+1);
   	shiftOut(DATA, SHIFT, LSBFIRST, ~arreglo[i]);
     shiftOut(DATA, SHIFT, LSBFIRST, 128 >> i);
+	unstore();
+    store();
   }
-  store();
 }
 
 
 void imagen(int a){ //funcion para mostrar un patron ingresado por el usuario
   
+  if(a==65){
   for(int i =0; i<8; i++){
     for(int j =0; j<8; j++){
         if(A[i][j]==1){
-            bitA[i] += pow(2,j);
+            bitA[i] += (int)pow(2,j);
         }
     }
   }
-  
-  if(a==65){
-  	encenderLed(bitA);
+ 
+  encenderLed(bitA);
+
 }
-  
-//for(int k=0; k<49; k++){
-//  if(LetrasNumeros[k] == a){
-//    encenderLed(LetrasMatriz[k]);
-//  }  
-    
-//}
+
   
 }
 
@@ -281,8 +267,10 @@ void verificacion ()
 {
   shiftOut(DATA, SHIFT, MSBFIRST, 0);
   shiftOut(DATA, SHIFT, MSBFIRST, 255);
+  unstore();
   store();
   shiftOut(DATA, SHIFT, MSBFIRST, 0);
+  unstore();
   store();
   
 }
@@ -292,13 +280,16 @@ void verificacion ()
 
 void store ()
 {  
-	digitalWrite (STORE, HIGH);
-    delayMicroseconds (100);
   	digitalWrite (STORE, LOW);
-    delayMicroseconds (10000);
+    delayMicroseconds (100000);
 
 }
 
+void unstore()
+{
+ 	digitalWrite (STORE, HIGH);
+    delayMicroseconds (100000);
+}
 
 
 
